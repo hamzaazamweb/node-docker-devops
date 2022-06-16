5b6502c8f4973cdc496654a0036d4a29d87cbc96
hamzaazam.web <hamzaazam.web@gmail.com>
2022-06-16 21:45:41 +0500

    added docker compose file

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h2>hi there</h2>");
+});
+
+console.log(process.env);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
