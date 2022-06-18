121b997a57f7cf2a2e388b4bfb99d5950c4440c5
hamzaazam.web <hamzaazam.web@gmail.com>
2022-06-18 16:27:32 +0500

    added config and env

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
-
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 mongoose
-  .connect("mongodb://root:password@mongo:27017/?authSource=admin")
+  .connect(mongoURL)
   .then(() => {
     console.log("connected to db");
   })
