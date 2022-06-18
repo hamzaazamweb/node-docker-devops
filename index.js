0520cd8a53ab8e066d25878ec06f703640d77eaf
hamzaazam.web <hamzaazam.web@gmail.com>
2022-06-18 17:02:40 +0500

    added simple post routes for our app

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
   MONGO_PORT,
 } = require("./config/config");
 
+const postRouter = require("./routes/postRoutes");
 const app = express();
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 mongoose
@@ -15,11 +16,12 @@ mongoose
     console.log("connected to db");
   })
   .catch((err) => console.log(err));
+
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("<h2>hi there!</h2>");
 });
-
-console.log(process.env);
+app.use("/posts", postRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
