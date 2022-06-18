7921bb39d1d5e1ffdd0c3d89c7a618889fca7050
hamzaazam.web <hamzaazam.web@gmail.com>
2022-06-18 15:58:00 +0500

    added mongoose

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose
+  .connect("mongodb://root:password@mongo:27017/?authSource=admin")
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => console.log(err));
 app.get("/", (req, res) => {
   res.send("<h2>hi there!</h2>");
 });
