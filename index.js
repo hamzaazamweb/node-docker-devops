60c0fb9a71ab9a4548c557051e623b880b08466b
hamzaazam.web <hamzaazam.web@gmail.com>
2022-06-19 20:26:05 +0500

    added authentication

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
 } = require("./config/config");
 
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 const app = express();
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 mongoose
@@ -21,7 +22,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("<h2>hi there!</h2>");
 });
-app.use("/posts", postRouter);
+app.use("/api/v1/posts", postRouter);
+app.user("/api/v1/users", userRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
